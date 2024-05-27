cef8d9ea64d68c87c59b75e6351586b56e9f02a6
Vicky <vickvuong23@gmail.com>
2024-05-27 14:40:32 +0700

    no message

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            abort(response()->json(["status"=> "Failed", "message" => "Bạn vui lòng đăng nhập"], 403));
         }
     }
 }
