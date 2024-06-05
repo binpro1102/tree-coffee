5cad942bdbcb5416f3411cf08b7377c793f79929
kien43 <kien4343@gmail.com>
2024-06-05 18:08:33 +0700

    CRUD mềm products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -60,7 +60,7 @@ class ProductController extends Controller
     public function get(Request $request)
     {
         try {
-            $product = Product::where('product_id', $request->input('product_id'))->firstOrFail();
+            $product = Product::where('product_id', $request->input('product_id'))->where('is_delete', false)->firstOrFail();
 
             return $this->responseCommon(200, "Lấy dữ liệu từ id thành công", $product);
 
@@ -113,13 +113,13 @@ class ProductController extends Controller
                 'unit_price' => 'required'
 
             ]);
-            $product = Product::findOrFail($request->input('product_id'));
+            $product = Product::where('product_id', $request->input('product_id'))->where('is_delete', false)->firstOrFail();
             $product->update($request->all());
 
             return $this->responseCommon(200, "Cập nhật sản phẩm thành công", $product);
         } catch (\Exception $e) {
 
-            return $this->responseCommon(400, "Cập nhật sản phẩm không thành công", null);
+            return $this->responseCommon(400, "Cập nhật sản phẩm không thành công, không tìm thấy id này", null);
         }
 
     }
@@ -130,14 +130,16 @@ class ProductController extends Controller
 
         try {
             $product = Product::findOrFail($request->input('product_id')); // lấy product_id truyền từ body để xóa
-            $product->is_delete = true;
-            $product->save();
 
+            if (!$product->is_delete) { // kiểm tra is_delete trong bảng là F hay không, nếu là F gán là T
+                $product->is_delete = true;
+                $product->save();
 
-            return $this->responseCommon(200, "sản phẩm đã được xóa thành công.", []);
-
+                return $this->responseCommon(200, "id đã được xóa thành công.", []);
+            } else {
+                return $this->responseCommon(404, "id đã được xóa trước đó", null);
+            }
         } catch (\Exception $e) {
-
             return $this->responseCommon(404, "không tìm thấy id trong cơ sở dữ liệu.", null);
         }
     }
